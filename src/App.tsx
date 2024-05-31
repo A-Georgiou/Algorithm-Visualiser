ef5beeb123b866dd77d267672509365ba3294739
Andrew Georgiou <andrewgeorgiou98@outlook.com>
2024-05-31 01:43:41 +0100

    Initial Commit - Algorithm Visualiser Project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from './components/Navbar.tsx';
+import './styles/main.css';
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+    </>
+  )
+}
+
+export default App
